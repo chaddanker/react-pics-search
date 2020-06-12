91020ce6e036505bfa5b00b91642956dd11add85
chaddanker <33150201+chaddanker@users.noreply.github.com>
2020-06-12 09:45:50 +0200

    Redacted API Key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
 			baseURL: 'https://api.unsplash.com',
 			headers: {
-				Authorization: 'Client-ID SUQ1D_-3F_Zb6n8qyd3ujpAI5T4BHZCynGtla6v8SU8'
+				Authorization: 'YOUR OWN UNSPLASH API KEY'
 			}
 });
 
